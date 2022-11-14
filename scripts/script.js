dcf988f3b4c99701387966821e5b28833314d6e9
fachriirzami <mfachrii2000@gmail.com>
2022-11-14 22:42:16 +0700

    content

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+// Change Menu Button
+let collapse = document.getElementById("collapsed")
+
+function changeToClose() {
+  collapse.innerHTML = 
+    '<span><img src="./assets/images/icon-menu-close.png" width="32px" height="32px" alt="close-button"></span>'
+  collapse.setAttribute('onclick','changeToMenu()')
+}
+
+function changeToMenu() {
+  collapse.innerHTML = 
+    '<span><img src="./assets/images/icon-menu.png" width="32px" height="32px" alt="collapsed-button"></span>'
+  collapse.setAttribute('onclick','changeToClose()')
+}
+
+// Back To Top Button
+let btn = document.getElementById("btnTop")
+
+window.onscroll = () => { scrollToShow() }
+
+function scrollToShow() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btn.style.display = "block"
+  } 
+  else { btn.style.display = "none" }
+}
+function backToTop() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
