b3b28c1d9523d9483495bee39f0a4bf141f40632
fachriirzami <50220983+fachriirzami@users.noreply.github.com>
2022-11-17 19:52:54 +0700

    content

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,47 @@
+// Refresh to top of the page
+if (history.scrollRestoration) {
+  history.scrollRestoration = 'manual';
+} 
+else {
+  window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+  }
+}
+
+// Switch Mode Button
+let switchMode = document.getElementById("switch-mode")
+let switchText = document.querySelectorAll(".switch-text")
+
+function darkMode() {
+  document.body.style.transition = "all .5s"
+  document.body.style.background = "black"
+
+  for (let i = 0; i < switchText.length; i++) {
+    switchText[i].style.setProperty("color", "white", "important");
+  }
+
+  switchMode.classList.remove("bg-dark", "text-light")
+  switchMode.classList.add("bg-light", "text-dark")
+
+  switchMode.innerHTML = "Light Mode"
+  switchMode.setAttribute('onclick','lightMode()')
+}
+
+function lightMode() {
+  document.body.style.transition = "all .5s"
+  document.body.style.background = "white"
+
+  for (let i = 0; i < switchText.length; i++) {
+    switchText[i].style.setProperty("color", "black", "important");
+  }
+
+  switchMode.classList.remove("bg-light", "text-dark")
+  switchMode.classList.add("bg-dark", "text-light")
+  
+  switchMode.innerHTML = "Dark Mode"
+  switchMode.setAttribute('onclick','darkMode()')
+}
+
 // Change Menu Button
 let btnCol = document.getElementById("collapse-button")
 let iconCol = document.getElementById("icon-collapse")
